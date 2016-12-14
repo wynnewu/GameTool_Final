f198986db8265d6d6bdf243782643e28ef0632d1
wynnewu <wynnmlgb@hotmail.com>
2016-12-13 18:22:27 -0800

    777

--- Game_Tool/Source.cpp
+++ Game_Tool/Source.cpp
@@ -11,70 +11,97 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include<cppunit/XmlOutputter.h>
 #include <cppunit/extensions/HelperMacros.h>
-//#define valueArr[40][3]
+#include <assert.h>
+#include "windows.h" 
+#include "zlib.h" 
+#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(__CYGWIN__)
+#  include <fcntl.h>
+#  include <io.h>
+#  define SET_BINARY_MODE(file) setmode(fileno(file), O_BINARY)
+#else
+#  define SET_BINARY_MODE(file)
+#endif
+
+
+#define CHUNK 16384
+
 
 using namespace std;
-/*double distanceCalculate(double valueArr[40][3]) {
+int def(FILE *source, FILE *dest, int level)
+{
+	int ret, flush;
+	unsigned have;
+	z_stream strm;
+	unsigned char in[CHUNK];
+	unsigned char out[CHUNK];
 
-	double minDis = 0;
-	double currentDis = 0;
-	double temp = 100;
+	/* allocate deflate state */
+	strm.zalloc = Z_NULL;
+	strm.zfree = Z_NULL;
+	strm.opaque = Z_NULL;
+	ret = deflateInit(&strm, level);
+	if (ret != Z_OK)
+		return ret;
 
-	int index1;
-	int index2;
-	int n = 0;
-	for (int i = 0; i < 40; i++)
-	{
-
-		currentDis = sqrt(pow(valueArr[i][0] - valueArr[i + 1][0], 2) + pow(valueArr[i][1] - valueArr[i + 1][1], 2) + pow(valueArr[i][2] - valueArr[i + 1][2], 2));
-
-
-		if (currentDis < temp)
-		{
-			temp = currentDis;
-			index1 = i;
-			index2 = i + 1;
+	/* compress until end of file */
+	do {
+		strm.avail_in = fread(in, 1, CHUNK, source);
+		if (ferror(source)) {
+			(void)deflateEnd(&strm);
+			return Z_ERRNO;
 		}
+		flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
+		strm.next_in = in;
 
-	}
-
-	double newArr[40][3];
-	for (int i = 0; i < 40; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			newArr[index1][j] = NULL;
-			newArr[index2][j] = NULL;
-
-			if (i != index1&&i != index2)
-			{
-				newArr[i][j] = valueArr[i][j];
+		/* run deflate() on input until output buffer not full, finish
+		compression if all of source has been read in */
+		do {
+			strm.avail_out = CHUNK;
+			strm.next_out = out;
+			ret = deflate(&strm, flush);    /* no bad return value */
+			assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+			have = CHUNK - strm.avail_out;
+			if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+				(void)deflateEnd(&strm);
+				return Z_ERRNO;
 			}
+		} while (strm.avail_out == 0);
+		assert(strm.avail_in == 0);     /* all input will be used */
 
+										/* done when last data in file processed */
+	} while (flush != Z_FINISH);
+	assert(ret == Z_STREAM_END);        /* stream will be complete */
+
+										/* clean up and return */
+	(void)deflateEnd(&strm);
+	return Z_OK;
+}
+
+
+int GetMaxCompressedLen(int nLenSrc)
+{
+	int n16kBlocks = (nLenSrc + 16383) / 16384; // round up any fraction of a block
+	return (nLenSrc + 6 + (n16kBlocks * 5));
+}
+/*
+int CompressData(const BYTE* abSrc, int nLenSrc, BYTE* abDst, int nLenDst)
+{
+	z_stream zInfo = { 0 };
+	zInfo.total_in = zInfo.avail_in = nLenSrc;
+	zInfo.total_out = zInfo.avail_out = nLenDst;
+	zInfo.next_in = (BYTE*)abSrc;
+	zInfo.next_out = abDst;
+
+	int nErr, nRet = -1;
+	nErr = deflateInit(&zInfo, Z_DEFAULT_COMPRESSION); // zlib function
+	if (nErr == Z_OK) {
+		nErr = deflate(&zInfo, Z_FINISH);              // zlib function
+		if (nErr == Z_STREAM_END) {
+			nRet = zInfo.total_out;
 		}
-
 	}
-
-
-	ofstream myfile("cba.txt");
-	myfile << "Min Distance: " << temp << endl;
-	myfile << "Row:" << index1<<", ";
-	myfile << "Row:" << index2<<endl;
-
-
-	for (int i = 0; i < 40; i++)
-	{
-		for (int j = 0; j < 3; j++) {
-
-				myfile << newArr[i][j] << " ";
-
-		}
-		myfile << endl;
-	}
-
-
-	return temp;
-
+	deflateEnd(&zInfo);    // zlib function
+	return(nRet);
 }
 */
 int main(int argc, char *argv[])
